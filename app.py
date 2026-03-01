82c9758509da3dd069dc5c074c75725d2c7112d6
kouchin05-cmd <kouchin05@gmail.com>
2026-03-01 14:41:19 +0900

    Use environment variable for port configuration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, abort
+import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def callback():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
